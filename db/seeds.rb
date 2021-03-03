7a98456342568780587e5e4342ea03c80d09c8b5
lindaounissi <linda.ounissi020712@gmail.com>
2021-03-03 13:24:10 +0100

    creating seeds of 5 restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+bristol = Restaurant.new(name: 'Epicure', category: 'french', address: '75008 Paris')
+pizzapai = Restaurant.new(name: 'Pizza', category: 'french', address: '69000 Lyon')
+elrancho = Restaurant.new(name: 'Tex Mex', category: 'french', address: '13011 Marseille')
+hippo = Restaurant.new(name: 'grill', category: 'french', address: '84000 Avignon')
+bureau = Restaurant.new(name: 'bistrot', category: 'french', address: '30000 Nimes')
+
+
